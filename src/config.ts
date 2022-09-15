0e08850f67ca1a85a5c6d888e8c08ff88b541709
Tony S <sanjayatony@gmail.com>
2022-09-15 10:06:24 +0700

    init

---
+++ src/config.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = "Tony S.";
+export const SITE_DESCRIPTION = "Nerd | Web Engineer | 👨🏻‍💻 + ☕️ = 🕸";
