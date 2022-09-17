60f9eaae08e72bcc763d276cdddb760e9544cac3
Tony S <sanjayatony@gmail.com>
2022-09-17 10:02:10 +0700

    use source sans pro

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,7 +3,7 @@ module.exports = {
 	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 	theme: {
 		fontFamily: {
-			sans: ["Inter", "sans-serif"],
+			sans: ["Source Sans Pro", "sans-serif"],
 			primary: ["Space Mono", "monospace"],
 		},
 		extend: {},
