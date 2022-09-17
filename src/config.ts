69f42ae8a315feb0b92afc27f07f4b4ed58b90a9
Tony S <sanjayatony@gmail.com>
2022-09-17 10:02:32 +0700

    change web description

--- src/config.ts
+++ src/config.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "Tony S.";
-export const SITE_DESCRIPTION = "Nerd | Web Engineer | 👨🏻‍💻 + ☕️ = 🕸";
+export const SITE_DESCRIPTION = "Nerd | Web Engineer | 👨🏻‍💻 + ☕️ ☕️ ☕️  = 🕸";
